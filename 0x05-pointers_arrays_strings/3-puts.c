2538372365dcf0da20f5c07148492f1900973a7e
Chidi820 <deborahchidi2@gmail.com>
2023-02-28 18:31:08 -0800

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,11 @@
   *__puts - prints a string,followed by a new line,to stdout)
  *@str: string to print
  */
-'void '_puts'(char * 'str')'i
+void  _ puts(char *str)
 {
 	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
-	       putchar('\n');
+	       _ putchar('\n');
 }
